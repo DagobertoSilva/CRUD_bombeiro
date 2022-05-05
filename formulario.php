62609969bc078da8fc505bc9f5974858b30a692f
DagobertoSilva <102236892+DagobertoSilva@users.noreply.github.com>
2022-05-05 10:08:35 -0300

    form css

--- formulario.php
+++ formulario.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
-
+<link rel="Stylesheet" type="text/css" href="formulario.css">
 
 
 	<title>FORMULÁRIO DE CADASTRO</title>
