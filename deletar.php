94430578ae487e67d620d4c50cd0824adf3ef114
DagobertoSilva <102236892+DagobertoSilva@users.noreply.github.com>
2022-05-05 09:56:04 -0300

    alteraçao deletar.php

--- deletar.php
+++ deletar.php
@@ -2,7 +2,7 @@
 include 'conexao.php';
 $id = $_GET['nmatricula'];
 
-if (isset( $_GET['id'])) {
+if (isset( $_GET['nmatricula'])){
 $sql = mysqli_query($conexaoBD, "DELETE FROM bombeiro WHERE nmatricula ={$id}");
 
  header('location: tabela.php');
