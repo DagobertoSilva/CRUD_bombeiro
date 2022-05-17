8437a083c40a5e036ce74d322261be74b2b2cdb8
DagobertoSilva <102236892+DagobertoSilva@users.noreply.github.com>
2022-05-17 07:36:31 -0300

    aletraçoes

--- tabela.php
+++ tabela.php
@@ -6,6 +6,7 @@ include 'listar.php';
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="Stylesheet" type="text/css" href="tabela.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js">
   </script>
